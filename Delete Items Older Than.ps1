83a6d01c1640e5d24987af672828c2dcc34c23e9
c-vohs <cvohs91@gmail.com>
2025-02-05 13:09:22 -0500

    Update Delete Items Older Than.ps1
    Add comments

--- Delete Items Older Than.ps1
+++ Delete Items Older Than.ps1
@@ -1,15 +1,24 @@
+# Written by Chris Vohs
+# Last Edited 2/5/25
+# Script will take an input $path and $DaysToKeep variables to check for items that are older than $DaysToKeep old.
+# Any items older will be purged and added to the log written in the same folder $Path.
+
 param([String]$Path, [Int]$DaysToKeep)
 
+# These are test variables
 #$Path = "C:\temp\test1"
 #[Int]$DaysToKeep = 1
 
 $logPath = Split-Path -Path $Path -Parent
 
+# Gets a list of items in the $Path and the $cutOffDate based on the $DaysToKeep
 $items = Get-ChildItem "$Path\*.*" 
 $cutOffDate = (get-date).adddays(-$DaysToKeep).Date
 
+# Initialize the logfile with current date
 get-date | out-file -Append "$logPath\deleteLog.txt"
 
+# Check each item in $items and remove if they are past the $cutOffDate. Write log of $item and last accessed date. If no items, just write to log.
 if ($items) {
     foreach ($item in $items) {
         if ($item.LastAccessTime -lt $cutOffDate) {
@@ -22,6 +31,7 @@ else {
     "No items in folder" | out-file -Append "$logPath\deleteLog.txt"
 }
 
+# Check if the log is too large and purge if it is.
 if (Test-Path "$logPath\deleteLog.txt") {
         if ((Get-item "$logPath\deleteLog.txt").Length -gt 3kb) {
             Remove-item "$logPath\deleteLog.txt"
